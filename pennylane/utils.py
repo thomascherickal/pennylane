b5e41c8809aa6f34b8428c0fddca158bd88c4b16
Christian Gogolin <git@cgogolin.de>
2019-01-18 21:15:05 +0100

    experimenting with a different approach

--- pennylane/utils.py
+++ pennylane/utils.py
@@ -64,7 +64,84 @@ def _flatten(x):
 
 
 def _unflatten(flat, model):
-    """Restores an arbitrary nested structure to a flattened iterable.
+    """Restores an arbitrary nested structure from a flattened iterable.
+
+    See also :func:`_flatten`.
+
+    Args:
+        flat (array): 1D array of items
+        model (array, Iterable, Number): model nested structure
+
+    Returns:
+        (other, array): first elements of flat arranged into the nested
+        structure of model, unused elements of flat
+    """
+    print("_unflatten("+str(flat)+", "+str(model)+")")
+    if isinstance(model, (numbers.Number, Variable)) and not isinstance(model, collections.Iterable):
+        print("branch: 0")
+        return flat[0], flat[1:]
+    elif isinstance(model, collections.Iterable):
+        print("branch 1")
+        if isinstance(model, np.ndarray):
+            l = model.shape[0] if model.shape != () else 0
+        else:
+            l = len(model)
+        print("l="+str(l))
+        if l == 0: #l=0 can only happen for single element np.arrays such as np.array(4) and for empty tuples
+            if isinstance(model, np.ndarray):
+                return np.array(flat[0], dtype=model.dtype), flat[1:]
+            else:
+                return (), flat
+
+        # Now we know that model is not just iterable, but can actually be indexed.
+
+        # For np.arrays there is a special shortcut if they are not yagged
+        # We try this after some sanity checks and hope for the best
+        if isinstance(model, np.ndarray):
+            size = model.size
+            if size == np.prod(model.shape) and np.all(x.size == model[0].size for x in model):
+                return np.array(flat)[:size].reshape(model.shape), flat[size:]
+
+        # As we have to unflatten depth-first, we need to keep track of the "tail"
+        # of not yet used elements but also want to yield from recursive calls of
+        # _unflatten(). The easies way to do this is via a class that keeps track
+        # of the "global" state of the tail while yielding:
+        class Unflattener:
+            def __init__(self, flat, model):
+                self.flat = flat
+                self.model = model
+                self.encountered_variable = False
+
+            def gen(self):
+                for i, x in enumerate(model):
+                    res, self.flat = _unflatten(self.flat, x)
+                    if isinstance(res, Variable):
+                        self.encountered_variable = True
+                    yield res
+
+            def tail(self):
+                print("self.flat="+str(self.flat))
+                return self.flat
+
+            def dtype(self):
+                if self.encountered_variable:
+                    return object
+                else:
+                    return self.model.dtype
+
+        unflattener = Unflattener(flat, model)
+
+        if isinstance(model, np.ndarray):
+            #return np.array(list(unflattener.gen()), dtype=unflattener.dtype()), unflattener.tail()
+            return np.fromiter(unflattener.gen(), dtype=unflattener.dtype()), unflattener.tail()
+        else:
+            return (type(model))(unflattener.gen()), unflattener.tail()
+    else:
+        raise TypeError('Unsupported type in the model: {}'.format(type(model)))
+
+
+def _unflatten_old(flat, model):
+    """Restores an arbitrary nested structure from a flattened iterable.
 
     See also :func:`_flatten`.
 
@@ -98,5 +175,5 @@ def unflatten(flat, model):
     # pylint:disable=len-as-condition
     res, tail = _unflatten(np.asarray(flat), model)
     if len(tail) != 0:
-        raise ValueError('Flattened iterable has more elements than the model.')
+        raise ValueError('Flattened iterable has more elements than the model. tail='+str(tail))
     return res
